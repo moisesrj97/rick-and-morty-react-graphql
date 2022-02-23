046e6c31ff736e2662b7932f5c4039b10d7d586b
moisesrj97 <moisesrj97@gmail.com>
2022-02-23 10:55:51 +0100

    Add footer with styles

--- src/core/Footer.tsx
+++ src/core/Footer.tsx
@@ -1,5 +1,20 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 
 export function Footer(): JSX.Element {
-  return <div>Footer</div>;
+  return (
+    <div className="text-white w-full">
+      <p className="text-center">
+        Made with 💚 by{' '}
+        <a
+          href="https://www.linkedin.com/in/moisesrodriguezjurado/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-green-400"
+        >
+          Moisés
+        </a>
+      </p>
+    </div>
+  );
 }
