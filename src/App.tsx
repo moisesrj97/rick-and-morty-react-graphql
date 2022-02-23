4c7a5425e0ab57dfb7dbd241a157fe566b6b8c2d
moisesrj97 <moisesrj97@gmail.com>
2022-02-23 10:39:21 +0100

    Insert NavBar and Footer in App

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,28 @@ import { LocationDetail } from './pages/LocationDetail';
 import { Episodes } from './pages/Episodes';
 import { EpisodeDetail } from './pages/EpisodeDetail';
 import { About } from './pages/About';
+import { NavBar } from './core/NavBar';
+import { Footer } from './core/Footer';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/characters/*" element={<Characters />} />
-        <Route path="/characters/:id" element={<CharacterDetail />} />
-        <Route path="/locations/*" element={<Locations />} />
-        <Route path="/locations/:id" element={<LocationDetail />} />
-        <Route path="/episodes/*" element={<Episodes />} />
-        <Route path="/episodes/:id" element={<EpisodeDetail />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+    <div className="App min-h-screen flex flex-col">
+      <NavBar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/characters/*" element={<Characters />} />
+          <Route path="/characters/:id" element={<CharacterDetail />} />
+          <Route path="/locations/*" element={<Locations />} />
+          <Route path="/locations/:id" element={<LocationDetail />} />
+          <Route path="/episodes/*" element={<Episodes />} />
+          <Route path="/episodes/:id" element={<EpisodeDetail />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+
+      <Footer />
     </div>
   );
 }
