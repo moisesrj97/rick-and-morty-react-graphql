b7ff3b468b43b4980e7e9a8cafd6dd42a455d5f9
moisesrj97 <moisesrj97@gmail.com>
2022-02-23 09:25:28 +0100

    Add routes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+
+import { Dashboard } from './pages/Dashboard';
+import { Characters } from './pages/Characters';
+import { CharacterDetail } from './pages/CharacterDetail';
+import { Locations } from './pages/Locations';
+import { LocationDetail } from './pages/LocationDetail';
+import { Episodes } from './pages/Episodes';
+import { EpisodeDetail } from './pages/EpisodeDetail';
+import { About } from './pages/About';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/characters/:id" element={<CharacterDetail />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/locations/:id" element={<LocationDetail />} />
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/episodes/:id" element={<EpisodeDetail />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
