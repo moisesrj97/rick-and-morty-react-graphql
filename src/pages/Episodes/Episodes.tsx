0dc59d9c49dabb8adb39546e939c22c723567fe5
moisesrj97 <moisesrj97@gmail.com>
2022-02-24 11:54:03 +0100

    Add episodes component

--- src/pages/Episodes/Episodes.tsx
+++ src/pages/Episodes/Episodes.tsx
@@ -1,5 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { Gallery } from '../../containers/Gallery/Gallery';
+import { NavButtons } from '../../core/NavButtons/NavButtons';
+
+interface EpisodesPagesAmountQueryI {
+  episodes: {
+    info: {
+      pages: number;
+    };
+  };
+}
+
+export const EPISODES_PAGES_AMOUNT_QUERY = gql`
+  query episodesPagesAmountQuery {
+    episodes {
+      info {
+        pages
+      }
+    }
+  }
+`;
 
 export function Episodes(): JSX.Element {
-  return <div>Episodes</div>;
+  const [pageIndex, setPageIndex] = useState(1);
+  const [contentLoading, setContentLoading] = useState(true);
+  const { data } = useQuery<EpisodesPagesAmountQueryI>(
+    EPISODES_PAGES_AMOUNT_QUERY
+  );
+
+  const nextPage = (): void => {
+    setPageIndex(pageIndex + 1);
+  };
+
+  const prevPage = (): void => {
+    setPageIndex(pageIndex - 1);
+  };
+
+  return (
+    <>
+      {data && !contentLoading && (
+        <NavButtons
+          nextPage={nextPage}
+          prevPage={prevPage}
+          currentPage={pageIndex}
+          maxPage={data.episodes.info.pages}
+        />
+      )}
+      <Gallery
+        pageIndex={pageIndex}
+        setContentLoading={setContentLoading}
+        type="Episodes"
+      />
+      {data && !contentLoading && (
+        <NavButtons
+          nextPage={nextPage}
+          prevPage={prevPage}
+          currentPage={pageIndex}
+          maxPage={data.episodes.info.pages}
+        />
+      )}
+    </>
+  );
 }
